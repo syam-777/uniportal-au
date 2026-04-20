6d4832ebfb40f2c096d9cecf062c6d6f283e49b8
syam-777 <syamuzumaki07@gmail.com>
2026-04-20 13:25:18 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,22 @@
-const CACHE_NAME = 'uniportal-au-v1';
+const CACHE_NAME = 'uniportal-au-v2';
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(['./index.html']);
+    })
+  );
+  // Force the new service worker to activate immediately
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
     })
   );
 });
